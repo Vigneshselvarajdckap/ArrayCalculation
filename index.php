f3a58a4b6c2b5dee0f1817d195241aeeb1f44faa
Vigneshselvarajdckap <vigneshselvarajdckap@gmail.com>
2023-05-22 11:10:16 +0530

    just update

--- index.php
+++ index.php
@@ -9,8 +9,8 @@ $arr =
     [12,2,22,8,-1]    // ans => 0
 ];
 
-$res = [];
-$maximum = [];
+$res = []; // show in all total values.
+$maximum = []; // show the maximum value in results.
 for ($i=0; $i <= count($arr)-1; $i++) 
 {
     $check = false;
